515adfad57bccbaf35c1e82a8c5e286bb438ce22
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2024-04-27 04:31:53 -0700

    Remove Tree class from TreeBot

--- src/greenscripter/minecraft/arun/TreeBot.java
+++ src/greenscripter/minecraft/arun/TreeBot.java
@@ -1,8 +1,6 @@
 package greenscripter.minecraft.arun;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,10 +14,8 @@ import greenscripter.minecraft.play.handler.PlayHandler;
 import greenscripter.minecraft.play.handler.PlayTickHandler;
 import greenscripter.minecraft.play.handler.WorldPlayHandler;
 import greenscripter.minecraft.utils.BlockBox;
-import greenscripter.minecraft.utils.Position;
 import greenscripter.minecraft.utils.Vector;
 import greenscripter.minecraft.world.PathFinder;
-import greenscripter.minecraft.world.World;
 import greenscripter.remoteindicators.IndicatorServer;
 
 public class TreeBot {
@@ -87,36 +83,4 @@ public class TreeBot {
 
 	}
 
-	static class Tree {
-
-		long chunk;
-		List<Position> blocks = new ArrayList<>();
-		BlockBox boundingBox;
-		int renderId;
-		String dimension;
-
-		public Tree(Set<Long> found, World w, Position start) {
-			List<Position> queue = new ArrayList<>();
-			queue.add(start.copy());
-			while (!queue.isEmpty()) {
-				Position next = queue.remove(queue.size() - 1);
-				int block = w.getBlock(next);
-				if (block >= 0 && logs[block]) {
-					if (found.add(next.getEncoded())) {
-						blocks.add(next);
-						for (int x = -1; x <= 1; x++) {
-							for (int y = -1; y <= 1; y++) {
-								for (int z = -1; z <= 1; z++) {
-									if (x == y && y == z && x == 0) continue;
-									queue.add(next.copy().add(x, y, z));
-								}
-							}
-						}
-					}
-				}
-			}
-			boundingBox = new BlockBox(blocks);
-		}
-
-	}
 }
