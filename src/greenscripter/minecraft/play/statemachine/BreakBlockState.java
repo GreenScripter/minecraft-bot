997008dad048aed365924b14bfce281acdc9a07e
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2024-05-05 02:09:07 -0700

    Made break block state select a reasonable tool.

--- src/greenscripter/minecraft/play/statemachine/BreakBlockState.java
+++ src/greenscripter/minecraft/play/statemachine/BreakBlockState.java
@@ -1,7 +1,12 @@
 package greenscripter.minecraft.play.statemachine;
 
+import greenscripter.minecraft.gameinfo.BlockStates;
+import greenscripter.minecraft.gameinfo.RegistryTags;
+import greenscripter.minecraft.play.data.InventoryData;
 import greenscripter.minecraft.play.data.PositionData;
 import greenscripter.minecraft.play.data.WorldData;
+import greenscripter.minecraft.play.inventory.ItemId;
+import greenscripter.minecraft.play.inventory.Slot;
 import greenscripter.minecraft.utils.Position;
 import greenscripter.minecraft.utils.Vector;
 import greenscripter.remoteindicators.IndicatorServer;
@@ -21,6 +26,29 @@ public class BreakBlockState extends PlayerState {
 			WorldData data = e.value.getData(WorldData.class);
 			PositionData pos = e.value.getData(PositionData.class);
 			if (render != null) shapeId = render.addCuboid(data.world.id, new Vector(block.x, block.y, block.z), new Vector(block.x + 1, block.y + 1, block.z + 1), IndicatorServer.getColor(255, 0, 0, 255));
+			InventoryData inv = e.value.getData(InventoryData.class);
+			String target = getBlockItemTag(data.world.getBlock(block));
+			Slot current = inv.getActiveScreen().getHotbarSlot(inv.hotbarSlot);
+			if ((target == null && (!current.present || !current.getItemInfo().isTool)) //
+					|| (target != null && current.present && ItemId.tags(target).contains(current.itemId))) {
+				//slot already reasonable
+			} else {
+				var it = inv.getActiveScreen().getHotbarIterator();
+				while (it.hasNext()) {
+					var next = it.next();
+					if (target == null) {
+						if (!next.present || !next.getItemInfo().isTool) {
+							inv.setHotbarSlot(inv.getActiveScreen().getHotbarIndex(next));
+							break;
+						}
+					} else {
+						if (next.present && ItemId.tags(target).contains(next.itemId)) {
+							inv.setHotbarSlot(inv.getActiveScreen().getHotbarIndex(next));
+							break;
+						}
+					}
+				}
+			}
 			data.startBreaking(e.value, block);
 			data.finishBreaking(e.value, block);
 			if (pos.getEyePos().distanceTo(new Vector(block)) >= 6) {
@@ -47,4 +75,23 @@ public class BreakBlockState extends PlayerState {
 
 	}
 
+	public static String getBlockItemTag(int blockId) {
+		String block = BlockStates.getState(blockId).block();
+		if (RegistryTags.matchesBlockTag("minecraft:mineable_pickaxe", block)) {
+			return "minecraft:pickaxes";
+		}
+		if (RegistryTags.matchesBlockTag("minecraft:mineable_shovel", block)) {
+			return "minecraft:shovels";
+		}
+		if (RegistryTags.matchesBlockTag("minecraft:mineable_axe", block)) {
+			return "minecraft:axes";
+		}
+		if (RegistryTags.matchesBlockTag("minecraft:mineable_hoe", block)) {
+			return "minecraft:hoes";
+		}
+		if (RegistryTags.matchesBlockTag("minecraft:sword_efficient", block)) {
+			return "minecraft:swords";
+		}
+		return null;
+	}
 }
